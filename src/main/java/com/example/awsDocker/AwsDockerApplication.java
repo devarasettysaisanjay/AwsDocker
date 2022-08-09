77b1887c7c45739ffc986edd05b858bafa1a0674
sanjay <devarasettysanjay9229@gmail.com>
2022-08-10 00:05:24 +0530

    making changes for the new branch

--- src/main/java/com/example/awsDocker/AwsDockerApplication.java
+++ src/main/java/com/example/awsDocker/AwsDockerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AwsDockerApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hi");;
+
 		SpringApplication.run(AwsDockerApplication.class, args);
 	}
 
